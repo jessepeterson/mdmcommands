4487e72b00e671b9648f7a94222161344ed6d0e7
Jesse Peterson <jesse.c.peterson@gmail.com>
2023-03-13 23:49:09 -0700

    rename for shared generate source; add DeviceInfo

--- generate.go
+++ generate.go
@@ -1,3 +1,4 @@
 package mdmcommands
 
 //go:generate admgencmd -pkg mdmcommands -o cmd_installmedia.go device-management/mdm/commands/media.install.yaml
+//go:generate admgencmd -pkg mdmcommands -o cmd_deviceinfo.go device-management/mdm/commands/information.device.yaml
